5875caae98a124b2a8b1c5d0f2280a41b9813286
Matt Block <matt@developmentblock.com>
2011-04-09 23:20:54 -0400

    Fixing tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 
 describe "Users" do
+  
   describe "signup" do
+    
     describe "failure" do
+      
       it "should not make a new user" do
         lambda do
           visit signup_path
@@ -14,14 +17,16 @@ describe "Users" do
           response.should render_template('users/new')
           response.should have_selector("div#error_explanation")
         end.should_not change(User, :count)
-      end
+      end 
     end
+    
     describe "success" do
+      
       it "should make a new user" do
         lambda do
           visit signup_path
-          fill_in "Name", :with => "Example User"
-          fill_in "Email", :with => "user@example.com"
+          fill_in "Name", :with => "Joe User"
+          fill_in "Email", :with => "joe.user@example.com"
           fill_in "Password", :with => "foobar"
           fill_in "Confirmation", :with => "foobar"
           click_button
@@ -29,6 +34,6 @@ describe "Users" do
           response.should render_template('users/show')
         end.should change(User, :count).by(1)
       end
-    end
+    end  
   end
 end
